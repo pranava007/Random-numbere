ebfc775582fbe69a70ce9b96eb2fe50c94d6f26d
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-26 00:30:59 +0530

    long double

---
+++ longdouble.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+int main()
+{
+    srand(time(0));
+    for (long double i=1 ; i <= 5; i=i+1)
+   
+    {
+     std::cout<< rand()<< i <<"\n" ;
+     
+    }
+  
+
+    return 0;
+}
